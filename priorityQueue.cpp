964187acbaaae354a4f95a6e125a35333ccca8da
Rushikesh Gherade <rushi422005@gmail.com>
2024-11-30 20:03:13 +0530

    Update priorityQueue.cpp
    MaxHeap and MinHeap

--- priorityQueue.cpp
+++ priorityQueue.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 int main(){
-    priority_queue<int> pq ;//MaxHeap
+    //priority_queue<int> pq ;//MaxHeap
     priority_queue<int, vector<int>, greater<int>> pq; // MinHeap
     pq.push(5);
     pq.push(3);
